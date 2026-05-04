c5b593e5585865ba9a123be019878d12341305ea
Raphaël Michnik <michnikraphael@gmail.com>
2026-05-04 18:57:23 -0400

    Distinct Numbers

---
+++ past/cses/distinct-numbers.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin >> n;
+    vector<int> a(n); 
+
+    for (int i=0; i<n; ++i) cin >> a[i];
+
+    set<int> distinct(a.begin(), a.end());
+    cout << distinct.size() << '\n';
+
+    return 0;
+}
