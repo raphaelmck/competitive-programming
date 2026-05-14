e494caf8689bd5721f8a5325d9aa50f7fb69cf7c
Raphaël Michnik <michnikraphael@gmail.com>
2026-05-14 16:51:09 -0400

    Minimizing Coins

---
+++ past/cses/minimizing-coins.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int INF = 1e9;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, x;
+    cin >> n >> x;
+
+    vector<int> coins(n);
+    for (int i=0; i<n; ++i) cin >> coins[i];
+
+    vector<int> dp(x+1, INF);
+    dp[0] = 0;
+
+    for (int i=1; i<=x; ++i) {
+        for (int c : coins) {
+            if (i - c >= 0) {
+                dp[i] = min(dp[i], dp[i - c] + 1);
+            }
+        }
+    }
+
+    cout << (dp[x] == INF ? -1 : dp[x]) << '\n';
+
+    return 0;
+}
