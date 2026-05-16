b64d491974353e871078138d42c0328055ce1b6c
Raphaël Michnik <michnikraphael@gmail.com>
2026-05-16 10:28:28 -0400

    Sum of Two Values

---
+++ past/cses/sum-of-two-values.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, x;
+    cin >> n >> x;
+
+    vector<pair<int ,int>> a(n);
+    for (int i=0; i<n; ++i) {
+        cin >> a[i].first;
+        a[i].second = i+1;
+    }
+
+    sort(a.begin(), a.end());
+
+    int l = 0, r = n-1;
+    while (l < r) {
+        int sum = a[l].first + a[r].first;
+        if (sum == x) {
+            cout << a[l].second << ' ' << a[r].second << '\n';
+            return 0;
+        } else if (sum < x) {
+            l++;
+        } else {
+            r--;
+        }
+    }
+
+    cout << "IMPOSSIBLE\n";
+
+    return 0;
+}
